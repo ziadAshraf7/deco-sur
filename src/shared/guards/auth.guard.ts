6c851b57c8d6bbfc3fcf59278ecdf2dabdbaf62b
ziad <91333254+ziadAshraf7@users.noreply.github.com>
2026-08-23 23:12:39 +0300

    feat : added user role checks inside auth guard

---
+++ src/shared/guards/auth.guard.ts
@@ -0,0 +1,76 @@
+// guards/jwt-auth.guard.ts
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { UsersService } from '../../users/users.service';
+import { AuthenticatedUserPayload } from '../../auth/dto/auth.dto';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from '@prisma/client';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService , 
+    private userService : UsersService , 
+    private reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+    const roles = this.getClassAndHandlerMetaData(context , "roles") as UserRole[]
+    
+    if (!token) {
+      throw new UnauthorizedException('No token provided');
+    }
+
+    try {
+      const payload : AuthenticatedUserPayload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      await this.validateUserAccess(payload.userId , payload.role , roles)
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  private getClassAndHandlerMetaData(context : ExecutionContext , metaDataKey : string){
+    return this.reflector.getAllAndOverride<string[]>(
+    metaDataKey,
+    [
+      context.getHandler(),
+      context.getClass(),
+    ],
+  );
+  }
+
+  private checkUserRole(userRole: UserRole, roles: UserRole[]): boolean {
+    return roles.includes(userRole);
+  }
+
+private async validateUserAccess(
+  userId: bigint,
+  userRole: UserRole,
+  roles: UserRole[],
+) {
+  const user = await this.userService.validateAndGetUser(userId);
+
+  if (!this.checkUserRole(userRole, roles)) {
+    throw new ForbiddenException('You do not have permission to access this resource');
+  }
+
+  return user;
+}}
