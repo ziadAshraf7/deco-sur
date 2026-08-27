6470630ff0c1d27febbaabd8cb2be2b65d4b0081
ziad <91333254+ziadAshraf7@users.noreply.github.com>
2026-08-27 15:00:55 +0300

    feat(users) : added users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PaginationQueryDto } from '../shared/dto/pagiantion.dto';
+import { AuthenticatedUserPayload } from '../auth/dto/auth.dto';
+import { CurrentUser } from '../shared/decerators/current_user.decerator';
+
+
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
 
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.usersService.create(dto);
+  }
+
+
+  @Get()
+  findAll(@Query() query: PaginationQueryDto) {
+    return this.usersService.findAll(query);
+  }
+
+
+  @Get('stats')
+  getStats() {
+    return this.usersService.getUserStats();
+  }
+
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findByIdOrThrow(BigInt(id));
+  }
+
+
+  @Patch('me')
+  update(
+    @Body() dto: UpdateUserDto,
+    @CurrentUser() user: AuthenticatedUserPayload,
+  ) {
+    return this.usersService.update(dto, user);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(BigInt(id));
+  }
 }
