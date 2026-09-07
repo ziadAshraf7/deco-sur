7e6dd3dbd6bc5bee41f8c05a86faea61d4e16b57
ziad <91333254+ziadAshraf7@users.noreply.github.com>
2026-09-07 22:16:34 +0300

    feat : listen to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
   app.enableShutdownHooks();
 
   const port = process.env.PORT ?? 8000;
-  await app.listen(port);
+  await app.listen(port , "0.0.0.0");
   logger.log(`🚀 Application running on: http://localhost:${port}`);
   logger.log(`📚 Swagger docs available at: http://localhost:${port}/docs`);
 }
